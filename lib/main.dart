3f50d12128090ac35142871c54121c797f4aa32c
Guilherme Gotardo <guilhermegotardo@outlook.com>
2026-03-16 20:30:49 -0300

    fix: add media kit player for Windows version

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'app/app_widget.dart';
+import 'package:media_kit/media_kit.dart';
+import 'package:video_player_media_kit/video_player_media_kit.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
+  MediaKit.ensureInitialized();
+  VideoPlayerMediaKit.ensureInitialized(
+    windows: true,
+  );
   runApp(const AppWidget());
 
 }
